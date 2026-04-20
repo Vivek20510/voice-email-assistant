ee02f2aa8c231e2ac401b2756107b293c4148463
Vivek20510 <vivekbadodiya68@gmail.com>
2026-04-20 23:05:21 +0530

    resolve merge conflicts with dev

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -37,6 +37,7 @@ class UserToken(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     service = db.Column(db.String(128), nullable=False)
+    account_email = db.Column(db.String(255), nullable=True)
     access_token = db.Column(db.Text, nullable=True)
     refresh_token = db.Column(db.Text, nullable=True)
     expires_at = db.Column(db.DateTime, nullable=True)
